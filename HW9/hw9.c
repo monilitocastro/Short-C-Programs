902e729f4fa4e3b48a9ac92cbec9f3b1caea3409
Live System User <liveuser@localhost.localdomain>
2015-11-19 23:37:05 -0600

    done

--- HW9/hw9.c
+++ HW9/hw9.c
@@ -1,18 +1,23 @@
 //MONILITO CASTRO HW9
-//Some of this source is borrowed http://stackoverflow.com/questions/3554120/open-directory-using-c
+//Some of this source is borrowed from http://stackoverflow.com/questions/3554120/open-directory-using-c
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
 
 int main (int c, char *v[]) {
         struct dirent *pointerFolderent;
 	DIR *pointerFolder;
-
+	FILE * stdout;
+        stdout = fopen("stdout1.txt", "w");
+        
+        /*
         if (c < 2) {
             	printf ("Usage: testprog <dirname>\n");
             	return 1;
-        }
-        pointerFolder = opendir (v[1]);
+        }*/
+        char dirName[] = "./";
+        pointerFolder = opendir (dirName);
         if (pointerFolder == 0) {
             	printf ("Directory error (cannot open): '%s'\n", v[1]);
             	return 1;
@@ -22,9 +27,17 @@ int main (int c, char *v[]) {
 		        strcmp(pointerFolderent->d_name, ".")==0){
 			continue;		
 		}
-       	     	printf ("%s\n", pointerFolderent->d_name);
+       	     	fputs(pointerFolderent->d_name, stdout );
+       	     	
+       	     	fputs("\n", stdout );
         }
 
         closedir (pointerFolder);
+        fclose(stdout );
+        system("sort stdout1.txt > stdout2.txt");
+        system("head -1 stdout2.txt; tail -1 stdout2.txt");
+        system("rm stdout1.txt  stdout2.txt");
+        
+        
         return 0;
     }
